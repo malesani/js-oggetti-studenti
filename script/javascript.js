58cc1584f1f4b8c5bb759335ed500a2e750e58ae
malesani <alejandroMalesani@hotmail.com>
2021-04-23 17:12:23 +0200

    finish

--- script/javascript.js
+++ script/javascript.js
@@ -4,12 +4,15 @@ var studente = {
   lastname : "malesani",
   age : 22
 }
+studente.segno = "bilancia"
 
 for(var key in studente){
   var studenteIn = "indice: "+ key + " valore: "+ studente[key];
   console.log(studenteIn)
 }
 
+//////////////////////////////////////
+console.log("")
 
 studenti = [
   {name : "juan",
@@ -28,14 +31,41 @@ studenti = [
   }
 ]
 
+
+var nombre = prompt("digitta il tuo nome")
+var apellido = prompt("digitta il cognome")
+var edad = prompt("digitta la tua etta")
+
+addStudente(studenti,nombre,apellido,edad)
+
+
 //stampa senza ciclo
+console.log("vedere tutto l'array")
 console.log(studenti);
+
 
 //stampa con ciclo 
 for(var dati of studenti){
+  console.log("")
+
   //vedo l'array con gli objetivi separatamente
-  console.log(dati)
+  for(var key in dati){
+    console.log("key : " + key + " valore: " + dati[key])
+  }
 }
+
+
+/////funzione-aggiungere dentro un array i dati dell'utente/////
+
+function addStudente(target, nombre, apellido, edad){
+  target.push({
+    name : nombre,
+    lastname : apellido,
+    age : edad
+  })
+}
+
+
 
   
 
